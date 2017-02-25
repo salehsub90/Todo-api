89a3e0655d2a06447734e43cae8a3f424edc1d99
Saleh Suboh <salehsuboh@gmail.com>
2017-02-25 08:38:23 -0500

    refactor API with uderscore

--- server.js
+++ server.js
@@ -21,14 +21,7 @@ app.get('/todos', function(req,res){
 // GET /todos/:id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	//iterate over todos array. find the math
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -39,8 +32,14 @@ app.get('/todos/:id', function(req, res){
 
 // POST /todos -- REST API
 app.post('/todos', function(req, res){
-	var body = req.body;
+	//var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
 
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
+	
 	//add id field
 	body.id = todoNextId++;
 	
