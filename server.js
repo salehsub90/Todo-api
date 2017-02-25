99c57a1482fc7dce1b12c10fa6a06fa59e651c71
Saleh Suboh <salehsuboh@gmail.com>
2017-02-25 09:36:03 -0500

    Add PUT todos

--- server.js
+++ server.js
@@ -39,11 +39,8 @@ app.post('/todos', function(req, res){
 		return res.status(400).send();
 	}
 	body.description = body.description.trim();
-
-	//add id field
 	body.id = todoNextId++;
 	
-	//push body into array
 	todos.push(body);
 
 	res.json(body);
@@ -60,6 +57,35 @@ app.delete('/todos/:id', function(req, res){
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
+
+});
+
+// PUT to update /todos/:id
+app.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	// HERE
+	_.extend(matchedTodo , validAttributes);
+	res.json(matchedTodo);
 
 });
 
