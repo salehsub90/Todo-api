6e879df009bc4d23ba14998d4498b0e7370e17b6
Saleh Suboh <salehsuboh@gmail.com>
2017-02-25 10:11:30 -0500

    Add variabl-playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person)
+// console.log(person);
+
+//Array Example:
+var grades = [15, 88];
+
+function addGrades(gradesArr){
+	grades.push(55);
+	debugger;
+	
+	//gradesArr = [12, 33 ,99];
+}
+
+addGrades(grades);
+console.log(grades);
