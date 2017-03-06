2906a0f0c8fbf69f6800e64919875f81780f1fde
Saleh Suboh <salehsuboh@gmail.com>
2017-03-05 19:26:40 -0500

    add login route and authenticate class mehtod

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
 
-module.exports = function (sequelize, DataTypes) {
-	return sequelize.define('user', {
+module.exports = function(sequelize, DataTypes) {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -24,7 +24,7 @@ module.exports = function (sequelize, DataTypes) {
 			validate: {
 				len: [7, 100]
 			},
-			set: function (value) {
+			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
 
@@ -35,17 +35,41 @@ module.exports = function (sequelize, DataTypes) {
 		}
 	}, {
 		hooks: {
-			beforeValidate: function (user, options) {
-				if ( typeof user.email === 'string') {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
 			}
 		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
+				});
+			}
+		},
 		instanceMethods: {
-			toPublicJSON: function () {
+			toPublicJSON: function() {
 				var json = this.toJSON();
-				return _.pick(json, 'id','email','updatedAt','createdAt');
+				return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
 			}
 		}
 	});
+
+	return user;
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -129,7 +130,20 @@ app.post('/users', function(req, res) {
 	});
 });
 
-db.sequelize.sync().then(function() {	//force: true will truncate current table and create new 1
+
+// POST /users/login
+app.post('/users/login', function (req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function () {
+		res.status(401).send();
+	});
+});
+
+
+db.sequelize.sync({force: true}).then(function() {	//(force: true) will truncate current table and create new 1
 	app.listen(PORT, function() {
 		console.log('express listening one port ' + PORT + '!');
 	});
