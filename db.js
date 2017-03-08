0596412f6e7e813762222414450a3052cfcfc63b
Saleh Suboh <salehsuboh@gmail.com>
2017-03-07 23:32:35 -0500

    Adding todo/user association

--- db.js
+++ db.js
@@ -21,8 +21,10 @@ var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.user = sequelize.import(__dirname + '/models/user.js');
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.todo.belongsTo(db.user);
+db.user.hasMany(db.todo);
 
 module.exports = db;
--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -21,44 +21,49 @@ var Todo = sequelize.define('todo', {
 });
 
 
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+//Associations and foriegn keys
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
 	//force: true
 }).then(function() {
 	console.log('everything synced');
 
-	Todo.findById(2).then(function(todo) {
-		if (todo) {
-			console.log(todo.toJSON());
-		} else {
-			console.log('id not found');
-		}
+	User.findById(1).then(function (user) {
+		user.getTodos({
+			where: {
+				completed: true
+			}
+		}).then(function (todos) {
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
 	});
 
-	// Todo.create({
-	// 	description: 'take out trash'
-	// 		//completed: false
-	// }).then(function(todo) {
+	// User.create({
+	// 	email: 'salehsuboh@gmail.com'
+	// }).then(function () {
 	// 	return Todo.create({
-	// 		description: 'Clean office'
+	// 		description: 'Clean Yard'
 	// 	});
-	// }).then(function() {
-	// 	//return Todo.findById(1);
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			description: {
-	// 				$like: '%Office%'
-	// 			}
-	// 		}
-	// 	})
-	// }).then(function(todos) {
-	// 	if (todos) {
-	// 		todos.forEach(function (todo) {
-	// 			console.log(todo.toJSON());
-	// 		});
+	// }).then(function (todo) {
+	// 	User.findById(1).then(function (user) {
+	// 		user.addTodo(todo);
+	// 	});
+	// });
+
+	// Todo.findById(2).then(function(todo) {
+	// 	if (todo) {
+	// 		console.log(todo.toJSON());
 	// 	} else {
-	// 		console.log('no todo found');
+	// 		console.log('id not found');
 	// 	}
-	// }).catch(function(e) {
-	// 	console.log(e)
 	// });
 });
